f5759942d9247f3b8ca23d09643debe1a70647cc
IgorChijov <igorchijof@gmail.com>
2024-06-09 17:19:15 +0300

    курсовая базовая

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,79 @@
+import java.util.Objects;
+
 public class Employee {
+
+    private static int idGenerator = 1;
+
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+    private final String middleName;
+    private int salary;
+    private int departmentNumber;
+
+    public Employee(String firstName, String lastName, String middleName, int departmentNumber, int salary) {
+        this.id = idGenerator++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.departmentNumber = departmentNumber;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public void setDepartmentNumber(int departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", salary=" + salary +
+                ", departmentNumber=" + departmentNumber +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(middleName, employee.middleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, middleName);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,73 @@
 public class Main {
+
+    private static Employee[] employees = new Employee[4];
+
+    public static void main(String[] args) {
+
+        employees[0] = new Employee("Ivan", "Ivanov", "Ivanovich", 1, 100000);
+        employees[1] = new Employee("Nikita", "Nikitin", "Nikitevich", 2, 11111);
+        employees[2] = new Employee("Pavel", "Popov", "Pavlovich", 3, 10000);
+        employees[3] = new Employee("Alexander", "Alexandrov", "Alexandrovich", 4, 15000);
+
+        printEmployees();
+        System.out.println("=========");
+        System.out.println(printSalary());
+        System.out.println("=========");
+        System.out.println(minSalary());
+        System.out.println("=========");
+        System.out.println(maxSalary());
+        System.out.println("=========");
+        System.out.println(averageSalary());
+        System.out.println("=========");
+        printFIO();
+        System.out.println("=========");
+
+    }
+
+
+
+
+    public static void printEmployees(){
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    public static int printSalary() {
+        int sum = 0;
+        for (Employee employee : employees){
+            sum += employee.getSalary();
+        }
+        return sum;
+    }
+
+    public static Employee minSalary() {
+        Employee min = employees[0];
+        for (Employee employee : employees) {
+            if (employee.getSalary() < min.getSalary()) {
+                min = employee;
+            }
+        }
+        return min;
+    }
+
+    public static Employee maxSalary() {
+        Employee max = employees[0];
+        for (Employee employee : employees) {
+            if (employee.getSalary() > max.getSalary()) {
+                max = employee;
+            }
+        }
+        return max;
+    }
+
+    public static double averageSalary() {
+        return (double) printSalary() / employees.length;
+    }
+
+    public static void printFIO() {
+        for (Employee employee : employees) {
+            System.out.println(employee.getLastName() + " " + employee.getFirstName() + " " + employee.getMiddleName());
+        }
+    }
 }
